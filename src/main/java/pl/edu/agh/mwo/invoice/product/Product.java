3fbc6d577c5fde94e1c39accff16bf7d160682b0
Marcin Micek <marcin.micek@gmail.com>
2018-02-03 11:05:21 +0100

    Invoice test product with null or empty name

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,6 +10,9 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if (name == null || name.isEmpty()) {
+			throw new IllegalArgumentException("Name is null or empty");
+		}
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
